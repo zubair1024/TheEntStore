c586d6346288d2cf0c92ba609c91bbdf909e4275
Zubair Ahmed <zubair1024@gmail.com>
2014-07-11 08:13:47 -0700

    0.4

--- inc/autoload.php
+++ inc/autoload.php
@@ -1,7 +1,14 @@
 <?php
 require_once('config.php');
 function __auto_load($class_name){
+ 
 	$class = eplode("_",$class_name);
 	$path = implode("/", $class).".php";
 	require_once($path);
+        
+    
+
+     
+ 
 }
+
--- inc/config.php
+++ inc/config.php
@@ -3,25 +3,35 @@ if(!isset($_SESSION)){
     session_start();
 }
 //website domain name with http
-defined("SITE_URL") || defined("SITE_URL","http://".$_SERVER['SERVER_NAME']);
+defined("SITE_URL")
+    || define("SITE_URL","http://".$_SERVER['SERVER_NAME']);
 //directory separator 
-defined("DS") || defined("DS",DIRECTORY_SEPARATOR);
+defined("DS")
+    || define("DS",DIRECTORY_SEPARATOR);
 //root path
-defined("ROOT_PATH") || defined("ROOT_PATH",  realpath(dirname(__FILE__).DS."..".DS));
+defined("ROOT_PATH") 
+    || define("ROOT_PATH",  realpath(dirname(__FILE__).DS."..".DS));
 //classes folder
-defined("CLASSES_DIR") || defined("CLASSES_DIR","classes");
+defined("CLASSES_DIR") 
+    || define("CLASSES_DIR","classes");
 //pages folder
-defined("PAGES_DIR") || defined("PAGES_DIR","pages");
+defined("PAGES_DIR") 
+    || define("PAGES_DIR","pages");
 //modules folder
-defined("MOD_DIR") || defined("MOD_DIR","mod");
+defined("MOD_DIR") 
+    || define("MOD_DIR","mod");
 //includes folder
-defined("INC_DIR") || defined("INC_DIR","inc");
+defined("INC_DIR") 
+    || define("INC_DIR","inc");
 //tempates folder
-defined("TEMPLATE_DIR") || defined("TEMPLATE_DIR","template");
+defined("TEMPLATE_DIR")
+    || define("TEMPLATE_DIR","template");
 //emails path
-defined("EMAILS_PATH") || defined("EMAILS_PATH",ROOT_PATH.DS."emails");
+defined("EMAILS_PATH")
+    || define("EMAILS_PATH",ROOT_PATH.DS."emails");
 //catalog images path
-defined("CATALOG_PATH") || defined("CATALOG_PATH",ROOT_PATH.DS."media".DS."catalog");
+defined("CATALOG_PATH")
+    || define("CATALOG_PATH",ROOT_PATH.DS."media".DS."catalog");
 
 //add all above directories to the include path
 set_include_path(implode(PATH_SEPARATOR, array(realpath(ROOT_PATH.DS.CLASSES_DIR),realpath(ROOT_PATH.DS.PAGES_DIR),realpath(ROOT_PATH.DS.MOD_DIR),realpath(ROOT_PATH.DS.INC_DIR),realpath(ROOT_PATH.DS.TEMPLATE_DIR),  get_include_path())));
